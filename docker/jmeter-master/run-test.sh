19c947233e605fe5bb6ddb518f3d3ecc3931c2f9
Karol Chrapek <kaarol@it-flow.pl>
2018-08-08 18:17:30 +0200

    Update docker image entrypoint

--- docker/jmeter-master/run-test.sh
+++ docker/jmeter-master/run-test.sh
@@ -1,9 +1,11 @@
-if [ -z ${SLAVE_IP_STRING} ]; then
-SLAVE_IP_STRING=`getent ahostsv4 ${SLAVE_SVC_NAME} |awk '!($1 in a){a[$1];printf "%s%s",t,$1; t=","}'`
-fi
-
 if [ "$ONE_SHOT" = "true" ]; then
-  for file in /test/*.jmx ; do
+  until [ $(find ${TESTS_DIR}/ -type f | wc -l) -ne 0 ]; do
+    sleep 20
+  done
+  if [ -z ${SLAVE_IP_STRING} ]; then
+  SLAVE_IP_STRING=`getent ahostsv4 ${SLAVE_SVC_NAME} |awk '!($1 in a){a[$1];printf "%s%s",t,$1; t=","}'`
+  fi
+  for file in ${TESTS_DIR}/*.jmx ; do
     jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED} -R ${SLAVE_IP_STRING}
   done
 else
